7f2aedadd3da1b4f0c1fec7aa577be5579b64bfc
damaris <mdamarisnduku@gmail.com>
2022-10-09 23:01:27 +0300

    commit tree f5e34bd45ef033589abf4473d8513e627157b647

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,57 @@
+# class UsersController < ApplicationController
+#     # skip_before_action :authorize, only: :create
+#     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+#     before_action :authorize, only: [:show]
+#     def create
+#         user = User.create(user_params)
+#         if user.valid?
+#             session[:user_id] = user.id
+#             render json: user, status: :created
+#         else
+#             render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+#         end
+#     end
+    
+#     def show
+#         user = User.find_by(id: session[:user_id])
+#         render json: user, status: :ok
+#     end
+#     private
+#     def authorize
+#         return render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
+#      end
+#     def user_params
+#         params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+#     end
+# end
+class UsersController < ApplicationController
+    # before_action :authenticate
+# rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_respons
+    wrap_parameters format: []
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user, status: :created
+        else
+            render json: {error: "Not authorized"}, status: :unauthorized
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+    end
+
+   
+end
